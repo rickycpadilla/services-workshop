647f77659acc7841a06c90cb245e5428b9e42636
Ricky Padilla <rickycpadilla@gmail.com>
2016-08-17 11:13:28 -0600

    completed

--- controllers/messages_controller.js
+++ controllers/messages_controller.js
@@ -1,4 +1,6 @@
-app.controller('MessagesController', function ($scope) {
+app.controller('MessagesController', function ($scope, MessagesService) {
   $scope.vw = {};
-  $scope.vw.success = "Success!!"
+  $scope.vw.success = "Success!!";
+  $scope.view = {};
+  $scope.view.messages = MessagesService.all
 })
--- controllers/post_message_controller.js
+++ controllers/post_message_controller.js
@@ -1,4 +1,15 @@
-app.controller('PostMessageController', function ($scope) {
+app.controller('PostMessageController', function ($scope, MessagesService, $window) {
   $scope.vw = {};
-  $scope.vw.success = "Success!!"
+  $scope.vw.success = "Success!!";
+  $scope.form = {};
+  $scope.submit = function(){
+    var message = {};
+    message.text = $scope.form.newmess
+    MessagesService.add(message);
+    $window.location.href = '/#/';
+  }
+  // $scope.submit($scope.form.message)
+  // function(){
+  //   console.log($scope.form.newmess);
+  // }
 })
